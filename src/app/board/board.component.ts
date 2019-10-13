32d0b7734560935db441c266701a21c8085cdad5
Yassine SABRI <ysabri@leyton.com>
2019-10-13 21:51:22 +0100

    basic logic and design

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+
+  squares: any[];
+  isXNext: boolean;
+  winner: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.isXNext = true;
+    this.winner = null;
+  }
+
+  get player() {
+    return this.isXNext ? 'X' : 'O';
+  }
+
+  makeMove(squareId : number) {
+    if (!this.squares[squareId]) {
+      this.squares.splice(squareId, 1, this.player);
+      this.isXNext = !this.isXNext;
+    }
+
+    this.winner = this.calculateWinner();
+  }
+
+  private calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+}
